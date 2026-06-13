9e5314bc31da5476266c0d8ec31cb82ca4e30abb
정성윤 <a50411278apeter4.0@gmail.com>
2026-06-13 09:54:34 +0000

    Auto-commit agent changes

---
+++ syblog_project/challenges/templatetags/challenge_extras.py
@@ -0,0 +1,8 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    if hasattr(dictionary, 'get'):
+        return dictionary.get(key)
+    return None
