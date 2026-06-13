8195b422ff8ebb091caac9f1b194f1eb4f3284c4
정성윤 <a50411278apeter4.0@gmail.com>
2026-06-13 10:12:34 +0000

    Add build.sh with admin account setup

--- syblog_project/build.sh
+++ syblog_project/build.sh
@@ -3,14 +3,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-# 관리자 계정 생성 (없으면)
-python manage.py shell -c "
+python manage.py shell << 'PYEOF'
 from django.contrib.auth.models import User
 from accounts.models import UserProfile
 if not User.objects.filter(username='admin').exists():
     u = User.objects.create_superuser('admin', 'admin@syblog.com', '1234aiai')
     UserProfile.objects.get_or_create(user=u)
-    print('관리자 계정 생성됨')
+    print('admin 계정 생성 완료')
 else:
-    print('관리자 계정 이미 존재')
-"
+    u = User.objects.get(username='admin')
+    u.set_password('1234aiai')
+    u.save()
+    UserProfile.objects.get_or_create(user=u)
+    print('admin 계정 확인 완료')
+PYEOF
